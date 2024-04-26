b72142e5bb6e8e928754640c0076f0c16c27518d
Wystan Melicor <131719849+StanMelix@users.noreply.github.com>
2024-04-26 15:28:40 -0500

    Getting started on power ups
    Added power up buttons, as well as rudimentary ammo/health buying methods with no functionality other than console printing

---
+++ Assets/BuyPowerUps.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyPowerUps : MonoBehaviour
+{
+    public void BuyAmmo()
+    {
+        Debug.Log("*buys pretend ammo*");
+        //HunterShoot.ammo += 20;
+    }
+
+    public void BuyHealth()
+    {
+        Debug.Log("*buys pretend health*");
+        //PlayerMovement.health += 1;
+    }
+}
